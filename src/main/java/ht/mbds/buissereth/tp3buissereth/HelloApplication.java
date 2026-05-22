277a83e9cbc75afb00db1e68da01ec54d5580ab6
Getro <156128381+getrobuissereth27@users.noreply.github.com>
2026-05-21 20:51:42 -0400

    état initial du projet

---
+++ src/main/java/ht/mbds/buissereth/tp3buissereth/HelloApplication.java
@@ -0,0 +1,9 @@
+package ht.mbds.buissereth.tp3buissereth;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/ht/mbds/buissereth/tp3buissereth/HelloResource.java
@@ -0,0 +1,14 @@
+package ht.mbds.buissereth.tp3buissereth;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
